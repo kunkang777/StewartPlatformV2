18bae58b97f35ea6c5ad6908d5880f41671cd555
Kun Kang <kun.kang@hotmail.com>
2023-02-21 00:00:47 -0800

    Add project files.

---
+++ StewartPlatformV2/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿
+namespace StewartPlatformV2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            this.trackBar4 = new System.Windows.Forms.TrackBar();
+            this.trackBar5 = new System.Windows.Forms.TrackBar();
+            this.trackBar6 = new System.Windows.Forms.TrackBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar7 = new System.Windows.Forms.TrackBar();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar7)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Position:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(108, 126);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Position:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(108, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Position:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(511, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Position:";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(511, 126);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Position:";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(511, 203);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Position:";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(111, 61);
+            this.trackBar1.Maximum = 135;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(361, 45);
+            this.trackBar1.TabIndex = 6;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(111, 142);
+            this.trackBar2.Maximum = 135;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(361, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(111, 219);
+            this.trackBar3.Maximum = 135;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Size = new System.Drawing.Size(361, 45);
+            this.trackBar3.TabIndex = 8;
+            this.trackBar3.Scroll += new System.EventHandler(this.trackBar3_Scroll);
+            // 
+            // trackBar4
+            // 
+            this.trackBar4.Location = new System.Drawing.Point(514, 61);
+            this.trackBar4.Maximum = 135;
+            this.trackBar4.Name = "trackBar4";
+            this.trackBar4.Size = new System.Drawing.Size(361, 45);
+            this.trackBar4.TabIndex = 9;
+            this.trackBar4.Scroll += new System.EventHandler(this.trackBar4_Scroll);
+            // 
+            // trackBar5
+            // 
+            this.trackBar5.Location = new System.Drawing.Point(514, 142);
+            this.trackBar5.Maximum = 135;
+            this.trackBar5.Name = "trackBar5";
+            this.trackBar5.Size = new System.Drawing.Size(361, 45);
+            this.trackBar5.TabIndex = 10;
+            this.trackBar5.Scroll += new System.EventHandler(this.trackBar5_Scroll);
+            // 
+            // trackBar6
+            // 
+            this.trackBar6.Location = new System.Drawing.Point(514, 219);
+            this.trackBar6.Maximum = 135;
+            this.trackBar6.Name = "trackBar6";
+            this.trackBar6.Size = new System.Drawing.Size(361, 45);
+            this.trackBar6.TabIndex = 11;
+            this.trackBar6.Scroll += new System.EventHandler(this.trackBar6_Scroll);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(111, 343);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(90, 59);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Test1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(111, 417);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 64);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "Test2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar7
+            // 
+            this.trackBar7.Location = new System.Drawing.Point(240, 383);
+            this.trackBar7.Maximum = 135;
+            this.trackBar7.Name = "trackBar7";
+            this.trackBar7.Size = new System.Drawing.Size(361, 45);
+            this.trackBar7.TabIndex = 15;
+            this.trackBar7.Scroll += new System.EventHandler(this.trackBar7_Scroll);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(385, 366);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Position:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(750, 383);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 17;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(977, 545);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.trackBar7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar6);
+            this.Controls.Add(this.trackBar5);
+            this.Controls.Add(this.trackBar4);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar7)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.TrackBar trackBar3;
+        private System.Windows.Forms.TrackBar trackBar4;
+        private System.Windows.Forms.TrackBar trackBar5;
+        private System.Windows.Forms.TrackBar trackBar6;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar7;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ StewartPlatformV2/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace StewartPlatformV2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            serialPort1.Open();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void SendServoInfo(int channel, int pos)
+        {
+            string message = channel.ToString() + ':' + pos.ToString() + '*';
+            try
+            {
+                serialPort1.Write(message);
+            }
+            catch
+            {
+
+            }
+        }
+        int minimumAngle = 0;
+        int maximumAngle = 135;
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            trackBar1.Minimum = minimumAngle;
+            trackBar1.Maximum = maximumAngle;
+            trackBar1.TickFrequency = 1;
+            int servoPos = trackBar1.Value;
+            if (serialPort1.IsOpen)
+            {
+                label1.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(0, servoPos);
+            }
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            trackBar2.Minimum = minimumAngle;
+            trackBar2.Maximum = maximumAngle;
+            trackBar2.TickFrequency = 1;
+            int servoPos = trackBar2.Value;
+            if (serialPort1.IsOpen)
+            {
+                label2.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(1, 180 - servoPos);
+            }
+        }
+
+        private void trackBar3_Scroll(object sender, EventArgs e)
+        {
+            trackBar3.Minimum = minimumAngle;
+            trackBar3.Maximum = maximumAngle;
+            trackBar3.TickFrequency = 1;
+            int servoPos = trackBar3.Value;
+            if (serialPort1.IsOpen)
+            {
+                label3.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(2, servoPos);
+            }
+        }
+
+        private void trackBar4_Scroll(object sender, EventArgs e)
+        {
+            trackBar4.Minimum = minimumAngle;
+            trackBar4.Maximum = maximumAngle;
+            trackBar4.TickFrequency = 1;
+            int servoPos = trackBar4.Value;
+            if (serialPort1.IsOpen)
+            {
+                label4.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(3, 180 - servoPos);
+            }
+        }
+
+        private void trackBar5_Scroll(object sender, EventArgs e)
+        {
+            trackBar5.Minimum = minimumAngle;
+            trackBar5.Maximum = maximumAngle;
+            trackBar5.TickFrequency = 1;
+            int servoPos = trackBar5.Value;
+            if (serialPort1.IsOpen)
+            {
+                label5.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(4, servoPos);
+            }
+        }
+
+        private void trackBar6_Scroll(object sender, EventArgs e)
+        {
+            trackBar6.Minimum = minimumAngle;
+            trackBar6.Maximum = maximumAngle;
+            trackBar6.TickFrequency = 1;
+            int servoPos = trackBar6.Value;
+            if (serialPort1.IsOpen)
+            {
+                label6.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(5, 180 - servoPos);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 0;
+            for (int j = 0; j < 10; j++)
+            {
+                for (int i = 1; i < 4; i++)
+                {
+                    Thread.Sleep(200);
+                    if (serialPort1.IsOpen)
+                    {
+                        if (j % 2 != 0)
+                        {
+                            a = 135;
+                        }
+                        else if (j % 2 == 0)
+                        {
+                            a = 45;
+                        }
+                        SendServoInfo(i * 2 - 2, a);
+                        SendServoInfo(i * 2 - 1, 180 - a);
+                        Console.WriteLine(i * 2 - 1);
+                        Console.WriteLine(i * 2);
+                        Console.WriteLine(j);
+                        Console.WriteLine(180 - j);
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int b = 0;
+            for (int j = 0; j < 5; j++)
+            {
+                Thread.Sleep(1000);
+                if (serialPort1.IsOpen)
+                {
+                    if (j % 2 != 0)
+                    {
+                        b = 135;
+                    }
+                    else if (j % 2 == 0)
+                    {
+                        b = 0;
+                    }
+                    SendServoInfo(0, b);
+                    SendServoInfo(1, 180 - b);
+                    SendServoInfo(2, b);
+                    SendServoInfo(3, 180 - b);
+                    SendServoInfo(4, b);
+                    SendServoInfo(5, 180 - b);
+                }
+            }
+        }
+
+        private void trackBar7_Scroll(object sender, EventArgs e)
+        {
+            trackBar7.Minimum = minimumAngle;
+            trackBar7.Maximum = maximumAngle;
+            trackBar7.TickFrequency = 1;
+            int servoPos = trackBar7.Value;
+            if (serialPort1.IsOpen)
+            {
+                label7.Text = "Position: " + servoPos.ToString();
+                label1.Text = "Position: " + servoPos.ToString();
+                label2.Text = "Position: " + servoPos.ToString();
+                label3.Text = "Position: " + servoPos.ToString();
+                label4.Text = "Position: " + servoPos.ToString();
+                label5.Text = "Position: " + servoPos.ToString();
+                label6.Text = "Position: " + servoPos.ToString();
+                SendServoInfo(0, servoPos);
+                SendServoInfo(1, 180 - servoPos);
+                SendServoInfo(2, servoPos);
+                SendServoInfo(3, 180 - servoPos);
+                SendServoInfo(4, servoPos);
+                SendServoInfo(5, 180 - servoPos);
+                trackBar1.Value = servoPos;
+                trackBar2.Value = servoPos;
+                trackBar3.Value = servoPos;
+                trackBar4.Value = servoPos;
+                trackBar5.Value = servoPos;
+                trackBar6.Value = servoPos;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
